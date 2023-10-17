216cda49b4760636169caa84d21536b723e76e32
Erblind Musliu <122228214+erblinnd@users.noreply.github.com>
2023-10-17 21:51:15 +0200

    first_commit
    streamlit input added

---
+++ kalkulimi_rroges.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def pay_weekly(hours, rate):
+    pay = hours*rate
+    return pay
+
+name = st.text_input("Shkruani emrin")
+hours = st.number_input("Shkruani oret")
+rate = st.number_input("Shkruani ratingun")
+result = pay_weekly(hours,rate)
+
+if st.button('Calculate wage!'):
+    st.write(f'{name} your wage is: ',result)
+
+
